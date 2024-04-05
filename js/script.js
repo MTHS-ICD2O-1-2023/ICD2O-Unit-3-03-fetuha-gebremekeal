49e332e7ca0fb9fcb3911716d9cb73130a8ec92b
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-04-05 18:32:53 +0000

    math

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fetuha
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates volume.
+ */
+function calculateVolumeOfSphere() {
+  // input
+  const radius = parseInt(document.getElementById('radius').value)
+
+
+  // process
+  const volumeOfPyramid = 4 / 3 * Math.PI * radius ** 3
+
+  // output
+  document.getElementById("volume").innerHTML = 'Volume is: ' + volumeOfPyramid.toFixed(2) + ' mm<sup>3'
+}
